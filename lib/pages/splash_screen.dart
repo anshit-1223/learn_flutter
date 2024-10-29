1cab2b21cc0ece6b2112f3ed42ccf9bb74a2b6f1
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2024-10-29 21:45:38 +0530

    Implemented SplashScreen Timer

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,7 +1,25 @@
+import 'dart:async';
+
+import 'package:day_01/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
