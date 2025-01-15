706a6550cd008cc649f384edf67b55cc32843af6
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2025-01-15 21:55:37 +0530

    Load and Decode Json from local files

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,36 @@
+import 'dart:convert';
+
 import 'package:day_01/models/catalog.dart';
 import 'package:day_01/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String ytchannel = "Codepur";
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
